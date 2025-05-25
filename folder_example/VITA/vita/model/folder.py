daf9d94937bafd852b34ce85082a443e1b5291e6
Anakin Skywalker <77543629+anakin-skywalker-Joseph@users.noreply.github.com>
2025-05-25 15:52:31 +0800

    Update folder.py

--- folder_example/VITA/vita/model/folder.py
+++ folder_example/VITA/vita/model/folder.py
@@ -131,8 +131,9 @@ def merge_features(image_features, metric=None, size=None, r=1, class_token=True
                                             class_token=class_token,
                                             )
         image_features, size, metric, _ = merge_folder(merge, image_features, size, metric)
-    log_size = 1 + size.log()
-    image_features = image_features * log_size
+    if r < image_features.shape[-2] // 2:
+        log_size = 1 + size.log()
+        image_features = image_features * log_size
     if is_batch == False:
         image_features = image_features.squeeze(0)
     return image_features, size, metric
